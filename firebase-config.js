6a988eaa236bb7f510b592a4de94276c7bcbb5fd
Erik Zhang <75038471+erikjzhang@users.noreply.github.com>
2025-10-25 18:23:36 -0400

    Allow firebase-config to be uploaded

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBL0k6AE5NBpVOQPEEGsKRMc48yDuEGonc",
+    authDomain: "ai-waste-classification-d07a6.firebaseapp.com",
+    projectId: "ai-waste-classification-d07a6",
+    storageBucket: "ai-waste-classification-d07a6.appspot.com",
+    messagingSenderId: "512015830235",
+    appId: "1:512015830235:web:cb0f09269ff5b9e8d010fe",
+    measurementId: "G-4T8NKD7K74"
+};
